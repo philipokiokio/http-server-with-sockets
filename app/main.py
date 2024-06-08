f0cb9a4a35205dc99ce9b60189466939ed3086fa
Philip <philipokiokio@gmail.com>
2024-06-08 11:28:16 +0100

    200 response

--- app/main.py
+++ app/main.py
@@ -9,7 +9,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    connection, address = server_socket.accept()
+    print(f"connected address: {address}")
+    connection.sendall(b"HTTP/1.1 200 OK \r\n\r\n")  # wait for client
 
 
 if __name__ == "__main__":
